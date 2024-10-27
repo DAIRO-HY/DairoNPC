c9bffaf903c4a0f4ca5d61a4d16759aef8eaec8e
zhoulq <615741631@qq.com>
2024-10-27 15:38:55 +0900

    添加自动编译脚本

--- document/docker-build/build-linux.sh
+++ document/docker-build/build-linux.sh
@@ -1,15 +1,87 @@
 #!/bin/bash
 
-#删除上次编译文件
-rm DairoNPC.zip
-rm -rf DairoNPC-main
-rm /app/DairoNPC/dairo-npc-linux-amd64
+#github登录票据
+github_token=$GITHUB_TOKEN
 
-curl -L -o DairoNPC.zip https://github.com/DAIRO-HY/DairoNPC/archive/refs/heads/main.zip?561
-unzip DairoNPC.zip
-cd DairoNPC-main
+#docker用户名
+docker_user=$DOCKER_USER
+docker_pwd=$DOCKER_PWD
 
-#开始编译
-go build -o /app/DairoNPC/dairo-npc-linux-amd64
+#项目名
+projectName="DairoNPC"
 
-/app/DairoNPC/dairo-npc-linux-amd64
+repo="DAIRO-HY/$projectName"
+branch="release"
+
+#最终编译好的二进制文件
+exec_name=dairo-npc-linux-amd64
+exec_file="./$exec_name"
+
+#--------------------------------------获取代码-----------------------------------------
+if [ -d $projectName ]; then
+    cd $projectName
+    git pull
+else
+    CLONE_URL="https://${github_token}@github.com/${repo}.git"
+    git clone --branch $branch $CLONE_URL
+    cd $projectName
+fi
+
+#---------------------------------------编译-----------------------------------------
+if [ -f $exec_file ]; then
+    rm $exec_file
+fi
+go build -o $exec_file
+if [ ! -e $exec_file ]; then
+    echo "编译失败"
+    exit 1
+fi
+
+
+#---------------------------------------创建标签----------------------------------------
+# 获取版本号
+version=$(grep -oP '(?<=VERSION = ")[^"]+' ./constant/NPCConstant.go)
+
+#删除本地已经存在的标签
+git tag -d $version
+
+#删除远程标签
+git push origin --delete tag $version
+
+git tag $version
+git push origin $version
+
+release_message="本次发布版本:$version"
+create_release_api_response=$(curl -L -X POST "https://api.github.com/repos/$repo/releases" \
+                        -H "Accept: application/vnd.github.v3+json" \
+                        -H "Authorization: Bearer $github_token" \
+                        -H "X-GitHub-Api-Version: 2022-11-28" \
+                        -d "{\"tag_name\":\"$version\",\"name\":\"$version\",\"body\":\"$release_message\"}")
+echo "创建标签结果:${create_release_api_response}"
+
+#通过正则匹配ReleaseId, [head -n 1]功能是从匹配到的多个字符串中去第一个字符串
+release_id=$(echo "$create_release_api_response" | grep -oP '(?<="id": )[^,]+' | head -n 1)
+echo "本地发布ID:${release_id}"
+
+
+#---------------------------------------上传编译好的二进制文件----------------------------------
+upload_file_api_response=$(curl -s -X POST \
+                            -H "Accept: application/vnd.github+json" \
+                            -H "Authorization: Bearer ${github_token}" \
+                            -H "X-GitHub-Api-Version: 2022-11-28" \
+                            -H "Content-Type: application/octet-stream" \
+                            --data-binary "@${exec_file}" \
+                            "https://uploads.github.com/repos/${repo}/releases/${release_id}/assets?name=${exec_name}")
+
+echo "上传文件结果:${upload_file_api_response}"
+
+
+#---------------------------------------上传Docker镜像-----------------------------------------
+mv $exec_file ./document/docker/
+cd ./document/docker/
+docker build -t $docker_user/dairo-npc:$version .
+docker login -u $docker_user --password $docker_pwd
+docker push $docker_user/dairo-npc:$version
+docker logout
+
+echo "---------------------------------------docker镜像推送完成--------------------------------------"
